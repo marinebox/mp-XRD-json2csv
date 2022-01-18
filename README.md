97098d34bdde5acd6d2de8401e0b7ca6cca9bba1
marinebox <60165927+marinebox@users.noreply.github.com>
2022-01-18 19:01:02 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mp-XRD-json2csv
+
+The XRD pattern which has been downloaded from Materials Project is JSON file.
+
+This python program convert the json file to csv file.
+
+## Attention
+
+This program ignores "hkl", "d_spacing" values, and X-ray source element and its wavelength.
+
+The output csv file has only two keys: `two-theta`, `amplitude`.
---
+++ mp-json2csv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from fileinput import filename
+import pandas as pd
+import json
+import os
+
+
+def json2csv(filename: str = None):
+    name = filename[:-5]
+    jsonFile = open(filename)
+    js = json.load(jsonFile)
+    patterns = js['pattern']
+    data = []
+    for pattern in patterns:
+        t = pattern[2]
+        a = pattern[0]
+        data.append([t, a])
+    df = pd.DataFrame(data, columns=['two_theta', 'amplitude'])
+    df.to_csv(name + '.csv')
+    return
+
+
+jsonFiles = [j for j in os.listdir('.') if j[-5:] == '.json']
+jsonFiles.sort()
+
+for i, e in enumerate(jsonFiles):
+    print(i, e)
+
+fileNum = int(input('which json file convert? enter number: '))
+filename = jsonFiles[fileNum]
+json2csv(filename)
+
+print('accomplished!')
